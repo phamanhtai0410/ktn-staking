9383c4a991010bb767e243b973d3feba06b28b32
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-10-15 16:33:34 +0700

    clear history packer

--- src/service/axiosClient.ts
+++ src/service/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-import { history ,  LocalStorageService } from "@/_helpers/";
+import {   LocalStorageService } from "@/_helpers/";
 const localStorageService = LocalStorageService.getService();
 
 const defaultHeader = {
@@ -115,7 +115,7 @@ const handleError = (error)  => {
 
 const clearAuthToken = () =>{
   LocalStorageService.clearToken();
-  history.push('/');
+  window.location.reload();  
 }
 
 export default axiosClient;
