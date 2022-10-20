97749b78a1bf473ed5fcddd8d2dd7344194b0363
Nguyen Huu Hanh <hanh.nguyen081191@gmail.com>
2022-10-20 11:06:13 +0700

    update wallet Disconnected

--- src/service/web3/use-easy-web3.ts
+++ src/service/web3/use-easy-web3.ts
@@ -32,6 +32,8 @@ export const useEasyWeb3 = (cb?: Web3Callback) => {
   }, [])
 
   useEffect(() => {
+    
+    // console.log("connectState",connectState);
     if(ConnectState.Connected === connectState){
       dispatch(setReducerWalletInfo({ 
         ...easyWeb3.getWalletInfo(),
@@ -39,6 +41,19 @@ export const useEasyWeb3 = (cb?: Web3Callback) => {
           easyWeb3
       }}))
     }
+
+    if(ConnectState.Disconnected === connectState){
+      // console.log("DEFAULT_WALLET_INFO",DEFAULT_WALLET_INFO);
+      dispatch(setReducerWalletInfo({ 
+        ...DEFAULT_WALLET_INFO,
+        ...{
+          easyWeb3:null,
+          address:null,
+          chainId:null,
+          balance:"0"
+      }}))
+    }
+
   }, [connectState])
 
   return { easyWeb3, connectState, walletInfo }
