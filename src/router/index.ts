14e713e1bf76575626ee6a4cbc477d0dd222045d
Tra <tradaik1806@gmail.com>
2022-11-01 14:22:38 +0700

    remove route referral

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,8 @@
 import StakingPage from '@/pages/home/index'
-import ReferralPage from '@/pages/referral/index'
 const routes = [
   {
     path: '/',
     component: StakingPage,
-  },
-  {
-    path: '/referral',
-    component: ReferralPage,
   },
 ]
 export default routes
